61d00341c742b7a419ea4584d9061e8f26550efc
OlegFil73 <104713527+OlegFil73@users.noreply.github.com>
2022-07-13 21:43:06 +0300

    ВЫПОЛНИЛ 4 ЗАДАНИЕ

---
+++ Lesson1/Ex004/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход 
+//три числа и выдаёт максимальное из этих чисел.
+int a = 5;
+int b = 3;
+int c = 7;
+
+int max = a;
+
+if (b > a)
+{
+    max = b;
+}
+else
+{
+    max = a;
+}
+
+if (c > max)
+{
+    Console.WriteLine(c);
+}
+else
+{
+    Console.WriteLine(max);
+}
