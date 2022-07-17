4f68bb0c72338d8f89ca5963d5566874e9a8f254
OlegFil73 <104713527+OlegFil73@users.noreply.github.com>
2022-07-17 20:07:11 +0300

    выполнил задание 3

---
+++ Lesson3/Ex021/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 21
+//Напишите программу, которая принимает на вход координаты двух точек и 
+//находит расстояние между ними в 3D пространстве.
+ Console.WriteLine("Ведите координату x точки A");
+int ax = Convert.ToInt32(Console.ReadLine());
+
+ Console.WriteLine("Ведите координату y точки A");
+int ay = Convert.ToInt32(Console.ReadLine());
+
+ Console.WriteLine("Ведите координату z точки A");
+int az = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ведите координату x точки B");
+int bx = Convert.ToInt32(Console.ReadLine());
+
+ Console.WriteLine("Ведите координату y точки B");
+int by = Convert.ToInt32(Console.ReadLine());
+
+ Console.WriteLine("Ведите координату z точки B");
+int bz = Convert.ToInt32(Console.ReadLine());
+
+double distance (int ax, int ay, int az, int bx, int by, int bz)
+{
+    int c = (ax - bx) * (ax - bx) + (ay - by) * (ay - by) + (az - bz) * (az - bz);
+    double result = Math.Sqrt(c);
+    return result;
+}
+double res = distance (ax, ay, az, bx, by, bz);
+Console.WriteLine($"{res} расстояние между A и B");
---
+++ Lesson3/Ex023/Program.cs
@@ -0,0 +1,10 @@
+﻿//Задача 23
+//Напишите программу, которая принимает на вход число (N) и
+// выдаёт таблицу кубов чисел от 1 до N.
+Console.WriteLine("Ведите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i < N + 1; i++)
+{
+    Console.WriteLine($"{i * i * i}");
+}
