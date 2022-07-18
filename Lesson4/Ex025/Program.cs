78c73d2b1b420d1026a80d6bdcf1674ffb726b25
OlegFil73 <104713527+OlegFil73@users.noreply.github.com>
2022-07-18 20:31:45 +0300

    выполнил 4 задание

---
+++ Lesson4/Ex025/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B.
+
+Console.WriteLine("Ведите число A");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ведите число B");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int c = a;
+for (int i = 1; i < b; i++)
+{
+    c = c * a;
+}
+Console.WriteLine("Число А в степени В равно");
+Console.WriteLine(c);
---
+++ Lesson4/Ex027/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и 
+// выдаёт сумму цифр в числе.
+
+Console.WriteLine("Ведите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sum = 0;
+while (number > 0)
+{
+    int temporary = number % 10;
+    sum = sum + temporary; 
+      number = number / 10;
+   }
+   
+Console.WriteLine("Сумма цифр в числе");
+Console.WriteLine(sum);
